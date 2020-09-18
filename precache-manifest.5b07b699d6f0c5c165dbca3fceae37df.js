fa9e13597695e4ee8c36762f9421b98c12c24c38
Max <maksimavramenko33@gmail.com>
2020-09-18 15:02:08 +0300

    Updates

---
+++ precache-manifest.5b07b699d6f0c5c165dbca3fceae37df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3e19d115f181417fc86af9e691fa47",
+    "url": "/HW5-react/index.html"
+  },
+  {
+    "revision": "1d25857cf102f7c32ca9",
+    "url": "/HW5-react/static/css/main.a719a105.chunk.css"
+  },
+  {
+    "revision": "034fd6eff21bbe599632",
+    "url": "/HW5-react/static/js/2.a7cc7e70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HW5-react/static/js/2.a7cc7e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d25857cf102f7c32ca9",
+    "url": "/HW5-react/static/js/main.184af2cb.chunk.js"
+  },
+  {
+    "revision": "db3333355695a90bb140",
+    "url": "/HW5-react/static/js/runtime-main.fb6fb764.js"
+  },
+  {
+    "revision": "d964e0b3c644193e5591758f01e91f68",
+    "url": "/HW5-react/static/media/padlock.d964e0b3.svg"
+  }
+]);
